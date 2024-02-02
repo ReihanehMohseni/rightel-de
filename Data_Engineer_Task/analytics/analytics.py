eda5c03d7cbd9ddb9a41b25d8b3ece34fc5c2931
Mohammad Forouhesh <reihaneh.mohseni2@gmail.com>
2024-02-02 12:49:04 +0330

    [FEAT] DAG added to code to process ETL automated.
    #5

--- Data_Engineer_Task/analytics/analytics.py
+++ Data_Engineer_Task/analytics/analytics.py
@@ -7,23 +7,24 @@ import pandas as pd
 import json
 from geopy.distance import geodesic
 
-load_dotenv()
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+from datetime import datetime, timedelta
 
-print('Waiting for the data generator...')
-sleep(2)
-print('ETL Starting...')
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2024, 1, 30),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+}
 
-while True:
-    try:
-        psql_engine = create_engine(environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
-        break
-    except OperationalError:
-        sleep(0.1)
-    sleep(10)
-    print(psql_engine)
-print('Connection to PostgresSQL successful.')
-
-# Write the solution here
+dag = DAG(
+    'device_data_etl',
+    default_args=default_args,
+    description='ETL pipeline for device data',
+    schedule_interval=timedelta(hours=1),
+)
 
 def calculate_distance(location1, location2):
     location1_dict = json.loads(location1)
@@ -32,54 +33,83 @@ def calculate_distance(location1, location2):
     lat2, lon2 = location2_dict['latitude'], location2_dict['longitude']
     return geodesic((lat1, lon1), (lat2, lon2)).kilometers
 
-start_datetime = "2024-01-31 18:00:00"
-end_datetime = "2024-01-31 18:59:59"
+def perform_etl(**kwargs):
+    print('ETL Starting...')
 
-query = text("""
-    SELECT 
-        device_id, 
-        temperature, 
-        location, 
-        TIMESTAMP 'epoch' + CAST(time AS BIGINT) * INTERVAL '1 second' as time 
-    FROM 
-        devices 
-    WHERE 
-        CAST(time AS BIGINT) >= EXTRACT(epoch FROM TIMESTAMP :start_datetime) 
-        AND CAST(time AS BIGINT) <= EXTRACT(epoch FROM TIMESTAMP :end_datetime)
-    ORDER BY 
-        device_id, 
-        location,
-        TIMESTAMP 'epoch' + CAST(time AS BIGINT) * INTERVAL '1 second' ASC
-""")
+    load_dotenv()
 
-query = query.bindparams(start_datetime=start_datetime, end_datetime=end_datetime)
+    while True:
+        try:
+            psql_engine = create_engine(environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
+            break
+        except OperationalError:
+            sleep(0.1)
+        sleep(10)
+        print(psql_engine)
+    print('Connection to PostgresSQL successful.')
+
+    # Write the solution here
+
+    execution_date = kwargs['execution_date']
+    start_datetime = execution_date.strftime('%Y-%m-%d %H:%M:%S')
+    end_datetime = (execution_date + timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S')
+
+    query = text("""
+        SELECT 
+            device_id, 
+            temperature, 
+            location, 
+            TIMESTAMP 'epoch' + CAST(time AS BIGINT) * INTERVAL '1 second' as time 
+        FROM 
+            devices 
+        WHERE 
+            CAST(time AS BIGINT) >= EXTRACT(epoch FROM TIMESTAMP :start_datetime) 
+            AND CAST(time AS BIGINT) <= EXTRACT(epoch FROM TIMESTAMP :end_datetime)
+        ORDER BY 
+            device_id, 
+            location,
+            TIMESTAMP 'epoch' + CAST(time AS BIGINT) * INTERVAL '1 second' ASC
+    """)
+
+    query = query.bindparams(start_datetime=start_datetime, end_datetime=end_datetime)
+
+    df = pd.read_sql_query(query, psql_engine)
+    df['time'] = df['time'].apply(lambda x: pd.to_datetime(x, unit='s'))
+    print(df)
+    aggregations = df.groupby('device_id').agg(
+        max_temperature=('temperature', 'max'),
+        data_points=('temperature', 'count'),
+        last_location=('location', 'last'),
+        last_time=('time', 'last')).reset_index()
+
+    last_location = None
+    total_distances = []
+    for _, row in aggregations.iterrows():
+        if last_location is None:
+            last_location = row['last_location']
+            total_distances.append(0)
+        else:
+            distance = calculate_distance(last_location, row['last_location'])
+            total_distances.append(distance)
+            last_location = row['last_location']
+    aggregations['total_distance'] = total_distances
+    print(aggregations)
+
+    mysql_engine = create_engine(environ["MYSQL_CS"], pool_pre_ping=True, pool_size=10)
+    with mysql_engine.connect() as mysql_conn:
+        aggregations.to_sql('aggregated_data', mysql_conn, if_exists='replace', index=False)
+
+    print('ETL Completed.')
 
 
+perform_etl_task = PythonOperator(
+    task_id='perform_etl_task',
+    python_callable=perform_etl,
+    provide_context=True,
+    dag=dag,
+)
 
-df = pd.read_sql_query(query, psql_engine)
-df['time'] = df['time'].apply(lambda x: pd.to_datetime(x, unit='s'))
-print(df)
-aggregations = df.groupby('device_id').agg(
-    max_temperature=('temperature', 'max'),
-    data_points=('temperature', 'count'),
-    last_location=('location', 'last'),
-    last_time=('time', 'last')).reset_index()
+perform_etl_task
 
-last_location = None
-total_distances = []
-for _, row in aggregations.iterrows():
-    if last_location is None:
-        last_location = row['last_location']
-        total_distances.append(0)
-    else:
-        distance = calculate_distance(last_location, row['last_location'])
-        total_distances.append(distance)
-        last_location = row['last_location']
-aggregations['total_distance'] = total_distances
-print(aggregations)
-
-mysql_engine = create_engine(environ["MYSQL_CS"], pool_pre_ping=True, pool_size=10)
-with mysql_engine.connect() as mysql_conn:
-    aggregations.to_sql('aggregated_data', mysql_conn, if_exists='replace', index=False)
-
-print('ETL Completed.')
+if __name__ == "__main__":
+    dag.cli()
